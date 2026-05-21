bb0bf8dc72ec613a0cf0ea6ead08e44b1e655f9e
Tomiris <tomirisnauryzbaj1@gmail.com>
2026-05-22 01:13:01 +0500

    feat(market): add core trading and redemption flows

--- src/PredictionMarket.sol
+++ src/PredictionMarket.sol
@@ -20,16 +20,28 @@ contract PredictionMarket {
     uint256 public yesReserve;
     uint256 public noReserve;
     uint256 public collateralPool;
+    uint256 public accruedFees;
+    uint256 public totalWinningSharesAtResolution;
+    uint256 public resolvedCollateralPool;
     uint8 public winningOutcome;
 
+    mapping(uint8 outcome => uint256 supply) public outcomeSupply;
+    mapping(address account => uint256 amount) public pendingCollateral;
+
     event SharesPurchased(address indexed buyer, uint8 indexed outcome, uint256 collateralIn, uint256 sharesOut);
+    event SharesSold(address indexed seller, uint8 indexed outcome, uint256 sharesIn, uint256 collateralOut);
     event MarketStateChanged(MarketTypes.MarketState previousState, MarketTypes.MarketState nextState);
     event MarketResolved(uint8 indexed outcome, uint256 timestamp);
+    event WinningsRedeemed(address indexed redeemer, uint256 sharesBurned, uint256 collateralOut);
+    event CollateralWithdrawn(address indexed account, uint256 amount);
 
     error MarketClosed();
     error InvalidOutcome();
     error ResolutionTooEarly();
     error InvalidOracleAnswer();
+    error InsufficientLiquidity();
+    error NothingToWithdraw();
+    error InvalidState();
 
     constructor(
         address treasury_,
@@ -54,7 +66,9 @@ contract PredictionMarket {
         if (outcome > OUTCOME_YES) revert InvalidOutcome();
         require(collateralIn > 0, "ZERO_COLLATERAL");
 
-        sharesOut = _quoteShares(outcome, collateralIn);
+        uint256 fee = _feeFor(collateralIn);
+        sharesOut = _quoteShares(outcome, collateralIn - fee);
+        accruedFees += fee;
         collateralPool += collateralIn;
 
         if (outcome == OUTCOME_YES) {
@@ -63,24 +77,76 @@ contract PredictionMarket {
             noReserve += sharesOut;
         }
 
+        outcomeSupply[outcome] += sharesOut;
         OUTCOME_TOKEN.mint(msg.sender, outcome, sharesOut);
         emit SharesPurchased(msg.sender, outcome, collateralIn, sharesOut);
     }
 
+    function sell(uint8 outcome, uint256 sharesIn) external returns (uint256 collateralOut) {
+        if (state != MarketTypes.MarketState.Trading) revert MarketClosed();
+        if (outcome > OUTCOME_YES) revert InvalidOutcome();
+        require(sharesIn > 0, "ZERO_SHARES");
+
+        collateralOut = _quoteCollateralOut(outcome, sharesIn);
+        if (collateralOut > collateralPool) revert InsufficientLiquidity();
+
+        uint256 fee = _feeFor(collateralOut);
+        collateralOut -= fee;
+        accruedFees += fee;
+        collateralPool -= collateralOut;
+
+        if (outcome == OUTCOME_YES) {
+            yesReserve -= sharesIn;
+        } else {
+            noReserve -= sharesIn;
+        }
+
+        outcomeSupply[outcome] -= sharesIn;
+        OUTCOME_TOKEN.burn(msg.sender, outcome, sharesIn);
+        pendingCollateral[msg.sender] += collateralOut;
+
+        emit SharesSold(msg.sender, outcome, sharesIn, collateralOut);
+    }
+
     function beginResolution() external {
         if (block.timestamp < marketConfig.endTime) revert ResolutionTooEarly();
+        if (state != MarketTypes.MarketState.Trading) revert InvalidState();
         _setState(MarketTypes.MarketState.PendingResolution);
     }
 
     function resolve() external {
-        if (state != MarketTypes.MarketState.PendingResolution) revert MarketClosed();
+        if (state != MarketTypes.MarketState.PendingResolution) revert InvalidState();
 
         (int256 answer,) = IOracleAdapter(marketConfig.oracleAdapter).latestAnswer(marketConfig.oracleQuestionId);
         if (answer != 0 && answer != 1) revert InvalidOracleAnswer();
 
         winningOutcome = answer == 0 ? OUTCOME_NO : OUTCOME_YES;
+        totalWinningSharesAtResolution = outcomeSupply[winningOutcome];
+        resolvedCollateralPool = collateralPool;
         _setState(MarketTypes.MarketState.Resolved);
         emit MarketResolved(winningOutcome, block.timestamp);
+    }
+
+    function redeemWinningShares(uint256 sharesIn) external returns (uint256 collateralOut) {
+        if (state != MarketTypes.MarketState.Resolved) revert InvalidState();
+        require(sharesIn > 0, "ZERO_SHARES");
+        require(totalWinningSharesAtResolution > 0, "NO_WINNING_SHARES");
+
+        collateralOut = (resolvedCollateralPool * sharesIn) / totalWinningSharesAtResolution;
+
+        outcomeSupply[winningOutcome] -= sharesIn;
+        OUTCOME_TOKEN.burn(msg.sender, winningOutcome, sharesIn);
+        pendingCollateral[msg.sender] += collateralOut;
+
+        emit WinningsRedeemed(msg.sender, sharesIn, collateralOut);
+    }
+
+    function withdrawCollateral() external returns (uint256 amount) {
+        amount = pendingCollateral[msg.sender];
+        if (amount == 0) revert NothingToWithdraw();
+
+        pendingCollateral[msg.sender] = 0;
+        emit CollateralWithdrawn(msg.sender, amount);
     }
 
     function snapshot() external view returns (MarketTypes.MarketSnapshot memory) {
@@ -95,11 +161,18 @@ contract PredictionMarket {
     }
 
     function _quoteShares(uint8 outcome, uint256 collateralIn) internal view returns (uint256) {
-        uint256 fee = (collateralIn * marketConfig.feeBps) / BPS_DENOMINATOR;
-        uint256 netCollateral = collateralIn - fee;
-
         uint256 activeReserve = outcome == OUTCOME_YES ? yesReserve : noReserve;
-        return (netCollateral * 1e18) / (activeReserve + 1e18);
+        return (collateralIn * 1e18) / (activeReserve + 1e18);
+    }
+
+    function _quoteCollateralOut(uint8 outcome, uint256 sharesIn) internal view returns (uint256) {
+        uint256 activeReserve = outcome == OUTCOME_YES ? yesReserve : noReserve;
+        require(activeReserve > sharesIn, "INSUFFICIENT_RESERVE");
+        return (sharesIn * (activeReserve + 1e18)) / 1e18;
+    }
+
+    function _feeFor(uint256 amount) internal view returns (uint256) {
+        return (amount * marketConfig.feeBps) / BPS_DENOMINATOR;
     }
 
     function _setState(MarketTypes.MarketState nextState) internal {
--- test/unit/PredictionMarketFactory.t.sol
+++ test/unit/PredictionMarketFactory.t.sol
@@ -1,44 +1,126 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {PredictionMarketFactory} from "../../src/PredictionMarketFactory.sol";
 import {OracleAdapter} from "../../src/OracleAdapter.sol";
 import {OutcomeToken} from "../../src/OutcomeToken.sol";
+import {PredictionMarket} from "../../src/PredictionMarket.sol";
+import {PredictionMarketFactory} from "../../src/PredictionMarketFactory.sol";
 import {TreasuryVault} from "../../src/TreasuryVault.sol";
 import {MarketTypes} from "../../src/libraries/MarketTypes.sol";
 
-contract PredictionMarketFactoryTest {
-    function testScaffoldCompiles() external pure returns (bool) {
-        return true;
+contract Trader {
+    function buy(PredictionMarket market, uint8 outcome, uint256 collateralIn) external returns (uint256) {
+        return market.buy(outcome, collateralIn);
     }
 
-    function exampleConfig(address collateral, address oracle) external pure returns (MarketTypes.MarketConfig memory) {
+    function sell(PredictionMarket market, uint8 outcome, uint256 sharesIn) external returns (uint256) {
+        return market.sell(outcome, sharesIn);
+    }
+
+    function redeem(PredictionMarket market, uint256 sharesIn) external returns (uint256) {
+        return market.redeemWinningShares(sharesIn);
+    }
+
+    function withdraw(PredictionMarket market) external returns (uint256) {
+        return market.withdrawCollateral();
+    }
+}
+
+contract PredictionMarketFactoryTest {
+    address internal constant OWNER = address(0xA11CE);
+    address internal constant COLLATERAL = address(0xCA11);
+
+    OutcomeToken internal outcomeToken;
+    OracleAdapter internal oracle;
+    TreasuryVault internal treasury;
+    PredictionMarketFactory internal factory;
+
+    function setUp() public {
+        outcomeToken = new OutcomeToken(address(this));
+        oracle = new OracleAdapter(address(this), 30 days);
+        treasury = new TreasuryVault(address(this));
+        factory = new PredictionMarketFactory(address(this), address(treasury), address(outcomeToken));
+    }
+
+    function testCreateMarketTracksAddress() public {
+        PredictionMarket market = _createMarket();
+
+        require(factory.marketCount() == 1, "bad count");
+        require(factory.marketAt(0) == address(market), "bad market");
+        require(market.FACTORY() == address(factory), "bad factory");
+    }
+
+    function testCreateMarketDeterministicStoresSalt() public {
+        MarketTypes.MarketConfig memory config = _config();
+        bytes32 salt = keccak256("market-1");
+
+        address market = factory.createMarketDeterministic(config, salt);
+
+        require(factory.marketBySalt(salt) == market, "salt missing");
+        require(factory.marketCount() == 1, "count mismatch");
+    }
+
+    function testBuyMintsOutcomeShares() public {
+        PredictionMarket market = _createMarket();
+        Trader trader = new Trader();
+
+        uint256 shares = trader.buy(market, market.OUTCOME_YES(), 1_000 ether);
+
+        require(shares > 0, "no shares");
+        require(market.collateralPool() == 1_000 ether, "pool mismatch");
+        require(outcomeToken.balanceOf(market.OUTCOME_YES(), address(trader)) == shares, "balance mismatch");
+        require(market.outcomeSupply(market.OUTCOME_YES()) == shares, "supply mismatch");
+    }
+
+    function testSellCreatesWithdrawableCollateral() public {
+        PredictionMarket market = _createMarket();
+        Trader trader = new Trader();
+
+        uint256 bought = trader.buy(market, market.OUTCOME_NO(), 2_000 ether);
+        uint256 soldOut = trader.sell(market, market.OUTCOME_NO(), bought / 2);
+        uint256 withdrawn = trader.withdraw(market);
+
+        require(soldOut > 0, "no collateral out");
+        require(withdrawn == soldOut, "withdraw mismatch");
+        require(market.pendingCollateral(address(trader)) == 0, "pending not cleared");
+    }
+
+    function testResolveAndRedeemWinningShares() public {
+        PredictionMarket market = _createMarket();
+        Trader yesTrader = new Trader();
+        Trader noTrader = new Trader();
+
+        uint256 yesShares = yesTrader.buy(market, market.OUTCOME_YES(), 1_500 ether);
+        noTrader.buy(market, market.OUTCOME_NO(), 500 ether);
+
+        oracle.setAnswer(keccak256("btc-100k-2026"), 1, block.timestamp);
+        market.beginResolution();
+        market.resolve();
+
+        uint256 redeemOut = yesTrader.redeem(market, yesShares / 2);
+        uint256 withdrawn = yesTrader.withdraw(market);
+
+        require(market.winningOutcome() == market.OUTCOME_YES(), "wrong winner");
+        require(market.totalWinningSharesAtResolution() == yesShares, "snapshot mismatch");
+        require(redeemOut > 0, "no redeem out");
+        require(withdrawn == redeemOut, "wrong withdraw");
+    }
+
+    function _createMarket() internal returns (PredictionMarket market) {
+        market = PredictionMarket(factory.createMarket(_config()));
+        outcomeToken.setMinter(address(market), true);
+    }
+
+    function _config() internal view returns (MarketTypes.MarketConfig memory) {
         return
             MarketTypes.MarketConfig({
                 question: "Will BTC be above 100k on 2026-12-31?",
-                endTime: 1_800_000_000,
+                endTime: uint64(block.timestamp),
                 resolveWindow: 1 days,
                 feeBps: 30,
-                collateralToken: collateral,
-                oracleAdapter: oracle,
+                collateralToken: COLLATERAL,
+                oracleAdapter: address(oracle),
                 oracleQuestionId: keccak256("btc-100k-2026")
             });
-    }
-
-    function exampleDeployment(
-        address owner
-    )
-        external
-        returns (
-            PredictionMarketFactory factory,
-            OutcomeToken outcomeToken,
-            OracleAdapter oracle,
-            TreasuryVault treasury
-        )
-    {
-        outcomeToken = new OutcomeToken(owner);
-        oracle = new OracleAdapter(owner, 1 days);
-        treasury = new TreasuryVault(owner);
-        factory = new PredictionMarketFactory(owner, address(treasury), address(outcomeToken));
     }
 }
